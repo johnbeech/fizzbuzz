fc42e0c86ee1b65b87f47a14f9b1c6ffde05e5d5
John Beech <jbeech@marqeta.com>
2021-12-01 13:41:20 +0000

    Refactor to group CLO tests together

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -16,60 +16,62 @@ describe('Fizz Buzz', () => {
     console.log = log
   })
 
-  it('should print out the number 1 to the console', () => {
-    fizzbuzz.run(1)
-    const actual = logs
-    const expected = [
-      ['1']
-    ]
-    expect(actual).to.deep.equal(expected)
-  })
+  describe('Command line output', () => {
+    it('should print out the number 1 to the console', () => {
+      fizzbuzz.run(1)
+      const actual = logs
+      const expected = [
+        ['1']
+      ]
+      expect(actual).to.deep.equal(expected)
+    })
 
-  it('should print out numbers, and the word Fizz for the number 3 to the console', () => {
-    fizzbuzz.run(3)
-    const actual = logs
-    const expected = [
-      ['1'],
-      ['2'],
-      ['Fizz']
-    ]
-    expect(actual).to.deep.equal(expected)
-  })
+    it('should print out numbers, and the word Fizz for the number 3 to the console', () => {
+      fizzbuzz.run(3)
+      const actual = logs
+      const expected = [
+        ['1'],
+        ['2'],
+        ['Fizz']
+      ]
+      expect(actual).to.deep.equal(expected)
+    })
 
-  it('should print out numbers, and the word Buzz for the number 5 to the console', () => {
-    fizzbuzz.run(5)
-    const actual = logs
-    const expected = [
-      ['1'],
-      ['2'],
-      ['Fizz'],
-      ['4'],
-      ['Buzz']
-    ]
-    expect(actual).to.deep.equal(expected)
-  })
+    it('should print out numbers, and the word Buzz for the number 5 to the console', () => {
+      fizzbuzz.run(5)
+      const actual = logs
+      const expected = [
+        ['1'],
+        ['2'],
+        ['Fizz'],
+        ['4'],
+        ['Buzz']
+      ]
+      expect(actual).to.deep.equal(expected)
+    })
 
-  it('should print out numbers, and the word FizzBuzz for numbers that are multiples of 3 and 5', () => {
-    fizzbuzz.run(15)
-    const actual = logs
-    const expected = [
-      ['1'],
-      ['2'],
-      ['Fizz'],
-      ['4'],
-      ['Buzz'],
-      ['Fizz'],
-      ['7'],
-      ['8'],
-      ['Fizz'],
-      ['Buzz'],
-      ['11'],
-      ['Fizz'],
-      ['13'],
-      ['14'],
-      ['FizzBuzz']
-    ]
-    expect(actual).to.deep.equal(expected)
+    it('should print out numbers, and the word FizzBuzz for numbers that are multiples of 3 and 5', () => {
+      fizzbuzz.run(15)
+      const actual = logs
+      const expected = [
+        ['1'],
+        ['2'],
+        ['Fizz'],
+        ['4'],
+        ['Buzz'],
+        ['Fizz'],
+        ['7'],
+        ['8'],
+        ['Fizz'],
+        ['Buzz'],
+        ['11'],
+        ['Fizz'],
+        ['13'],
+        ['14'],
+        ['FizzBuzz']
+      ]
+      expect(actual).to.deep.equal(expected)
+    })
   })
 
   describe('Edge cases', () => {
